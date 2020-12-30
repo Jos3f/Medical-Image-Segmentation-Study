555881e4fe3cc7b3d786404473cbba31b9e19914
Jos3f <Jos3f@users.noreply.github.com>
2020-12-30 20:33:32 +0100

    Cleanup

--- unet/unet_experiment_scilife.py
+++ unet/unet_experiment_scilife.py
@@ -106,7 +106,7 @@ def remove_border(predictions, desired_dim_x, desired_dim_y):
     predictions = predictions[:, border_x:(width - border_x), border_y:(height - border_y), :]
     return predictions
 
-def main(start_index=0, filename=None, plot_validation=False, plot_test=True, calculate_train_metric=False):
+def main(filename=None, calculate_train_metric=False):
     """
 
     :param start_index:
@@ -182,218 +182,89 @@ def main(start_index=0, filename=None, plot_validation=False, plot_test=True, ca
     # for i in range(len(scilife_images)):
     #     print(np.max(scilife_images[i]))
 
-    for test_data_point_index in range(start_index, start_index+1):
-        print("\nStarted for data_point_index: " + str(test_data_point_index))
+    images_temp = images.copy()
+    labels_temp = labels.copy()
 
-        images_temp = images.copy()
-        labels_temp = labels.copy()
-
-        """for i in range((5)):
-            plt.matshow(images_temp[i][..., -1])
-            plt.show()
-            plt.matshow(np.argmax(labels_temp[i], axis=-1), cmap=plt.cm.gray)
-            plt.show()"""
-
-        test_image = images_temp.pop(test_data_point_index)
-        test_label = labels_temp.pop(test_data_point_index)
-
-        test_dataset = tf.data.Dataset.from_tensor_slices(([test_image],
-                                                           [test_label]))
-
-        print("num images: " + str(len(images_temp)))
-        print("num labels: " + str(len(labels_temp)))
-
-        random_permutation = np.random.permutation(len(images_temp))
-        images_temp = np.array(images_temp)[random_permutation]
-        labels_temp = np.array(labels_temp)[random_permutation]
-
-        image_dataset = tf.data.Dataset.from_tensor_slices((images_temp, labels_temp))
-
-        """Crate data splits"""
-        data_augmentation = tf.keras.Sequential([
-            tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
-            tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
-        ])
-
-        # image_dataset.shuffle(100, reshuffle_each_iteration=False)
-
-        train_dataset = image_dataset.take(80)
-        validation_dataset = image_dataset.skip(80)
-
-        train_dataset.shuffle(80, reshuffle_each_iteration=True)
-
-        train_dataset = train_dataset.map(augment_image) # Apply transformations to training data
-
-
-        """Load model"""
-        print(circles.channels)
-        print(circles.classes)
-
-        unet_model = unet.build_model(channels=circles.channels,
-                                      num_classes=circles.classes,
-                                      layer_depth=3,
-                                      filters_root=16)
-        if calculate_train_metric:
-            unet.finalize_model(unet_model)
-        else:
-            unet.finalize_model(unet_model, dice_coefficient=False, auc=False, mean_iou=False)
-
-
-        """Train"""
-        # callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
-        # trainer = unet.Trainer(checkpoint_callback=False, callbacks=[callback])
-        trainer = unet.Trainer(checkpoint_callback=False)
-        # trainer.fit(unet_model,
-        #             train_dataset,
-        #             validation_dataset,
-        #             epochs=25,
-        #             batch_size=1)
-        trainer.fit(unet_model,
-                    train_dataset,
-                    epochs=25,
-                    batch_size=1)
-
-        """Calculate best amplification using validation set"""
-        prediction = unet_model.predict(validation_dataset.batch(batch_size=1))
-
-        original_images = []
-        metric_labels = []
-        metric_predictions_unprocessed = []
-        metric_predictions = []
-
-        # print("Validation shape before: ", prediction.shape)
-        # dataset = validation_dataset.map(utils.crop_image_and_label_to_shape(prediction.shape[1:]))
-        dataset = validation_dataset.map(utils.crop_image_and_label_to_shape((inp_dim, inp_dim, 2)))
-        prediction = remove_border(prediction, inp_dim, inp_dim)
-        # print("Validation shape after: ", prediction.shape)
-
-
-        for i, (image, label) in enumerate(dataset):
-            original_images.append(image[..., -1])
-            metric_labels.append(np.argmax(label, axis=-1))
-            metric_predictions_unprocessed.append(prediction[i, ...])
-
-
-        threshold_util = Threshold(metric_labels)
-        """Skip thresholding output, use no threshold on this data"""
-        best_tau = 1
-        best_score = 1
-        # best_tau, best_score = threshold_util.get_best_threshold(metric_predictions_unprocessed, min=0, max=0.02, num_steps=200, metric=1)
-        print("Best tau: " + str(best_tau))
-        print("Best avg score: " + str(best_score))
-
-        for i in range(len(metric_predictions_unprocessed)):
-            metric_predictions.append(np.argmax(metric_predictions_unprocessed[i] * np.array([[[1, best_tau]]]), axis=-1))
-
-        if plot_validation:
-            fig, ax = plt.subplots(3, 3, sharex=True, sharey=True, figsize=(8, 8))
-
-            for i in range(3):
-                ax[i][0].matshow(original_images[i])
-                ax[i][1].matshow(metric_labels[i], cmap=plt.cm.gray)
-                ax[i][2].matshow(metric_predictions[i], cmap=plt.cm.gray)
-
-            plt.tight_layout()
-            plt.show()
-
-        original_images = []
-        metric_labels = []
-        metric_predictions_unprocessed = []
-        metric_predictions = []
-
-
-        """Evaluate and print to file"""
-        prediction = unet_model.predict(test_dataset.batch(batch_size=1))
-        # print("Test shape before: ", prediction.shape)
-        # dataset = test_dataset.map(utils.crop_image_and_label_to_shape(prediction.shape[1:]))
-        dataset = test_dataset.map(utils.crop_image_and_label_to_shape((inp_dim, inp_dim, 2)))
-        prediction = remove_border(prediction, inp_dim, inp_dim)
-        print("Test shape shape: ", prediction.shape)
-
-
-
-
-        for i, (image, label) in enumerate(dataset):
-            original_images.append(image[..., -1])
-            metric_labels.append(np.argmax(label, axis=-1))
-            metric_predictions_unprocessed.append(prediction[i, ...])
-
-        for i in range(len(metric_predictions_unprocessed)):
-            metric_predictions.append(np.argmax(metric_predictions_unprocessed[i] * np.array([[[1, best_tau]]]), axis=-1))
-
-        quantitative_metrics = Metrics(metric_labels, metric_predictions)
-
-        jaccard_index = quantitative_metrics.jaccard()
-        dice = quantitative_metrics.dice()
-        adj = quantitative_metrics.adj_rand()
-        warping_error = quantitative_metrics.warping_error()
-        # warping_error = [0.1]
-
-        with results_file.open("a") as f:
-            f.write(str(test_data_point_index) + ";" + str(jaccard_index[0]) + ";" + str(dice[0])
-                    + ";" + str(adj[0]) + ";" + str(warping_error[0]) + "\n")
-
-        print("test_data_point_index: " + str(test_data_point_index))
-        print("Jaccard index: " + str(jaccard_index))
-        print("Dice: " + str(dice))
-        print("Adj: " + str(adj))
-        print("Warping Error: " + str(warping_error))
-
-
-        """Plot predictions of synthetic test set"""
-        if plot_test:
-            fig, ax = plt.subplots(1, 3, figsize=(8, 4))
-            fig.suptitle("Test point: " + str(test_data_point_index), fontsize=14)
-
-
-            ax[0].matshow(original_images[i])
-            ax[0].set_title("Input data")
-            ax[0].set_axis_off()
-
-            ax[1].matshow(metric_labels[i], cmap=plt.cm.gray)
-            ax[1].set_title("True mask")
-            ax[1].set_axis_off()
-
-            ax[2].matshow(metric_predictions[i], cmap=plt.cm.gray)
-            ax[2].set_title("Predicted mask")
-            ax[2].set_axis_off()
-
-
-            fig.tight_layout()
-            plt.show()
-
-        """Sci Life data prediction"""
-        scilife_dataset = tf.data.Dataset.from_tensor_slices((scilife_images, scilife_labels))
-        prediction = unet_model.predict(scilife_dataset.batch(batch_size=1))
-
-        original_images = []
-        metric_labels = []
-        metric_predictions_unprocessed = []
-        metric_predictions = []
-
-        dataset = scilife_dataset.map(utils.crop_image_and_label_to_shape((inp_dim, inp_dim, 2)))
-        prediction = remove_border(prediction, inp_dim, inp_dim)
-        # print("Validation shape after: ", prediction.shape)
-
-        for i, (image, _) in enumerate(dataset):
-            original_images.append(image[..., -1])
-            metric_predictions_unprocessed.append(prediction[i, ...])
-
-        for i in range(len(metric_predictions_unprocessed)):
-            metric_predictions.append(
-                np.argmax(metric_predictions_unprocessed[i] * np.array([[[1, best_tau]]]), axis=-1))
-
-        fig, ax = plt.subplots(5, 2, sharex=True, sharey=True, figsize=(25, 60))
-
-        for i in range(5):
-            ax[i][0].matshow(original_images[i])
-            ax[i][1].matshow(metric_predictions[i], cmap=plt.cm.gray)
-            plt.imsave("results/scilifelab_" + str(current_dt) + "_index_" + str(i) + ".png", metric_predictions[i], cmap=plt.cm.gray)
-
-        plt.tight_layout()
-        plt.savefig("results/scilifelab_" + str(current_dt) +".png")
+    """for i in range((5)):
+        plt.matshow(images_temp[i][..., -1])
         plt.show()
+        plt.matshow(np.argmax(labels_temp[i], axis=-1), cmap=plt.cm.gray)
+        plt.show()"""
+
+    print("num images: " + str(len(images_temp)))
+    print("num labels: " + str(len(labels_temp)))
+
+    random_permutation = np.random.permutation(len(images_temp))
+    images_temp = np.array(images_temp)[random_permutation]
+    labels_temp = np.array(labels_temp)[random_permutation]
+
+    image_dataset = tf.data.Dataset.from_tensor_slices((images_temp, labels_temp))
+
+    """Crate data splits"""
+    train_dataset = image_dataset.take(100)
+    validation_dataset = image_dataset.skip(100)
+
+    train_dataset.shuffle(100, reshuffle_each_iteration=True)
+
+    train_dataset = train_dataset.map(augment_image) # Apply transformations to training data
+
+
+    """Load model"""
+    print(circles.channels)
+    print(circles.classes)
+
+    unet_model = unet.build_model(channels=circles.channels,
+                                  num_classes=circles.classes,
+                                  layer_depth=3,
+                                  filters_root=16)
+    if calculate_train_metric:
+        unet.finalize_model(unet_model)
+    else:
+        unet.finalize_model(unet_model, dice_coefficient=False, auc=False, mean_iou=False)
+
+
+    """Train"""
+    # callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+    # trainer = unet.Trainer(checkpoint_callback=False, callbacks=[callback])
+    trainer = unet.Trainer(checkpoint_callback=False)
+
+    trainer.fit(unet_model,
+                train_dataset,
+                epochs=25,
+                batch_size=1)
+
+    """Sci Life data prediction"""
+    scilife_dataset = tf.data.Dataset.from_tensor_slices((scilife_images, scilife_labels))
+    prediction = unet_model.predict(scilife_dataset.batch(batch_size=1))
+
+    original_images = []
+    metric_labels = []
+    metric_predictions_unprocessed = []
+    metric_predictions = []
+
+    dataset = scilife_dataset.map(utils.crop_image_and_label_to_shape((inp_dim, inp_dim, 2)))
+    prediction = remove_border(prediction, inp_dim, inp_dim)
+    # print("Validation shape after: ", prediction.shape)
+
+    for i, (image, _) in enumerate(dataset):
+        original_images.append(image[..., -1])
+        metric_predictions_unprocessed.append(prediction[i, ...])
+
+    for i in range(len(metric_predictions_unprocessed)):
+        metric_predictions.append(
+            np.argmax(metric_predictions_unprocessed[i] * np.array([[[1, 1]]]), axis=-1))
+
+    fig, ax = plt.subplots(5, 2, sharex=True, sharey=True, figsize=(25, 60))
+
+    for i in range(5):
+        ax[i][0].matshow(original_images[i])
+        ax[i][1].matshow(metric_predictions[i], cmap=plt.cm.gray)
+        plt.imsave("results/scilifelab_" + str(current_dt) + "_index_" + str(i) + ".png", metric_predictions[i], cmap=plt.cm.gray)
+
+    plt.tight_layout()
+    plt.savefig("results/scilifelab_" + str(current_dt) +".png")
+    plt.show()
 
 
 if __name__ == '__main__':
-    main(start_index=0, filename="results/SciLife.csv", plot_validation=True, plot_test=True, calculate_train_metric=False)
+    main(filename="results/SciLife.csv", calculate_train_metric=False)
